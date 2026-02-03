d7283069e82bc9f62e3bb83d2b286b9bc7d6b85a
sirisha-k83 <52533913+sirisha-k83@users.noreply.github.com>
2026-02-03 21:21:28 +0000

    Remove backend configuration from provider.tf
    Removed backend configuration for azurerm provider.

--- Terraform/1.provider.tf
+++ Terraform/1.provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "4.10.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "Common-RG"  #Shd be created before 
-    storage_account_name = "mywebsitestorage"  #Shd be created before
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
