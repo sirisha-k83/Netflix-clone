7294e28da7d7a8a7bbcfe710385c434af7d94de6
sirisha-k83 <52533913+sirisha-k83@users.noreply.github.com>
2026-03-13 20:50:59 +0000

    Configure Azure backend for Terraform state
    Add backend configuration for Azure storage

--- Terraform/1.provider.tf
+++ Terraform/1.provider.tf
@@ -5,13 +5,18 @@ terraform {
       version = "4.10.0"
     }
   }
+
+ backend "azurerm" {
+   resource_group_name  = "AZB48SLB"
+   storage_account_name = "londonstorageindia"
+   container_name       = "tfstate"
+   key                  = "aks.tfstate"
+ }
 }
 
 provider "azurerm" {
+ subscription_id = "4879f329-b467-46e8-9912-49f66c238cdd"
   features {}
 }
-
-
-
 
 
